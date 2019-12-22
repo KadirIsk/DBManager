22da2f278f8629b226fefbd0606b75f366f92d48
kadir.isik <kadir.isik@mebitech.com>
2019-12-22 20:50:27 +0300

    trim and split functions added

--- DBManager/Utility.cpp
+++ DBManager/Utility.cpp
@@ -9,14 +9,12 @@
 void Utility::setAppDir(std::string* dbDir)
 {
 	std::string propFileDir("C:/Users/kadir.isik/Desktop/Desktop/VS/DBManager/DBManager/dbmanager-properties.txt");
-	std::string* fileName = new std::string(propFileDir);
-	std::string* delimiter = new std::string("=");
-	std::string* line = new std::string();
+	auto fileName = new std::string(propFileDir);
+	auto delimiter = new std::string("=");
+	auto line = new std::string();
 	std::list<std::string*> lines;
-	std::list<Property*> properties;
 
-	std::fstream* propertyFile;
-	propertyFile = FileService::openFileReadingMode(fileName, std::ios_base::in);
+	auto propertyFile = FileService::openFileReadingMode(fileName, std::ios_base::in);
 
 	if (FileService::isOpen(propertyFile)) {
 		while (std::getline(*propertyFile, *line)) {
@@ -24,8 +22,78 @@ void Utility::setAppDir(std::string* dbDir)
 				lines.push_back(line);
 			}
 		}
-		properties = PropertyService::parse(lines, delimiter);
+		std::string* propName = new std::string("db_dir");
+		auto properties = PropertyService::parse(lines, delimiter);
+		auto property = PropertyService::findProperty(properties, propName);
+		if (!ValidationService::isNull(property)) {
+			*dbDir = *property->getValue();
+		}
 	}
 
 	FileService::closeFile(propertyFile);
 }
+
+void Utility::trim(std::string* str)
+{
+	rtrim(str);
+	ltrim(str);
+}
+
+void Utility::ltrim(std::string* str)
+{
+	if (!ValidationService::isNull(str) && !ValidationService::isEmpty(str)) {
+		int begin = 0;
+		int curIndx = 0;
+		for (auto it = str->begin(); it != str->end(); it++, curIndx++) {
+			if (*it != ' ' && *it != '\t' && *it != '\n') {
+				begin = curIndx;
+				break;
+			}
+		}
+
+		*str = str->substr(begin, str->length() - 1);
+	}
+}
+
+void Utility::rtrim(std::string* str)
+{
+	if (!ValidationService::isNull(str) && !ValidationService::isEmpty(str)) {
+		int end = 0;
+		int curIndx = str->length();
+		for (auto rit = str->rbegin(); rit != str->rend(); rit++, curIndx--) {
+			if (*rit != ' ' && *rit != '\t' && *rit != '\n') {
+				end = curIndx;
+				break;
+			}
+		}
+
+		*str = str->substr(0, end);
+	}
+}
+
+std::list<std::string*> Utility::split(std::string* str, std::string* delimiter)
+{
+	if (ValidationService::isNull(str) || 
+		ValidationService::isEmpty(str) ||
+		ValidationService::isNull(delimiter) || 
+		ValidationService::isEmpty(delimiter)) {
+		return std::list<std::string*>();
+	}
+	size_t pos = 0;
+	std::string* token;
+	std::list<std::string*> list;
+	auto copy = *str;
+	while ((pos = copy.find(*delimiter)) != std::string::npos) {
+		token = new std::string(copy.substr(0, pos));
+		copy.erase(0, pos + 1);
+		list.push_back(token);
+	}
+	
+	if (copy.length() > 0) {
+		list.push_back(new std::string(copy));
+	}
+
+	return list;
+}
+
+
--- DBManager/Utility.h
+++ DBManager/Utility.h
@@ -1,8 +1,13 @@
 #pragma once
 #include <string>
+#include <list>
 class Utility
 {
 public:
 	static void setAppDir(std::string*);
+	static void trim(std::string*);
+	static void ltrim(std::string*);
+	static void rtrim(std::string*);
+	static std::list<std::string*> split(std::string*, std::string*);
 };
 
